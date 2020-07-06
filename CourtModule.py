7ecd551c592147df1e8604e967a62e8eaaa0ba9a
fulva1230 <b10503137@gapps.ntust.edu.tw>
2020-07-06 13:16:53 +0800

    init commit

---
+++ CourtModule.py
@@ -0,0 +1,18 @@
+import torch
+
+class CourtNet(torch.nn.Module):
+    def __init__(self, jurors, class_num):
+        super(CourtNet, self).__init__()
+        self.jurors = jurors
+        self.class_num = class_num
+        l1_output_size = class_num * len(jurors) // 2
+        self.l1 = torch.nn.Linear(class_num * len(jurors), l1_output_size)
+        self.f1 = torch.nn.functional.relu
+        self.l2 = torch.nn.Linear(l1_output_size, class_num)
+        def f2(input_tensor):
+            return torch.nn.functional.softmax(input_tensor, 1)
+        self.f2 = f2
+
+    def forward(self, x):
+        jurors_pred = torch.cat([juror(x) for juror in self.jurors], 1)
+        return self.f2(self.l2(self.f1(self.l1(jurors_pred))))
