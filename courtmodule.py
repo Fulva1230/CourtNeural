4fe32f6708fdae8f36815a9ade38bf2c978276d8
fulva1230 <b10503137@gapps.ntust.edu.tw>
2020-07-08 16:35:38 +0800

    change struc of CourtNet

--- courtmodule.py
+++ courtmodule.py
@@ -5,16 +5,22 @@ class CourtNet(torch.nn.Module):
         super(CourtNet, self).__init__()
         self.jurors = jurors
         self.class_num = class_num
-        l1_output_size = class_num * len(jurors) // 2
+        l1_output_size = class_num * len(jurors)
         self.l1 = torch.nn.Linear(class_num * len(jurors), l1_output_size)
         self.f1 = torch.nn.functional.relu
-        self.l2 = torch.nn.Linear(l1_output_size, class_num)
+        l2_output_size = l1_output_size // 1.5
+        self.l2 = torch.nn.Linear(l1_output_size, l2_output_size)
+
+        self.f2 = torch.nn.functional.relu
+        self.l3 = torch.nn.Linear(l2_output_size, class_num)
+        def finalf(input_tensor):
+            return torch.nn.functional.softmax(input_tensor, 1)
+        self.finalf = finalf
         self.params1 = self.l1.parameters()
         self.params2 = self.l2.parameters()
-        def f2(input_tensor):
-            return torch.nn.functional.softmax(input_tensor, 1)
-        self.f2 = f2
+        self.params3 = self.l3.parameters()
+
 
     def forward(self, x):
         jurors_pred = torch.cat([juror(x) for juror in self.jurors], 1)
-        return self.f2(self.l2(self.f1(self.l1(jurors_pred))))
+        return self.finalf(self.l3(self.f2(self.l2(self.f1(self.l1(jurors_pred))))))
