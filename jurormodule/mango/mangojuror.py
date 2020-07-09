1f15ab8b106a780cf04e45c90912a779f758edaf
fulva1230 <b10503137@gapps.ntust.edu.tw>
2020-07-09 15:50:29 +0800

    change struc of Juror

--- jurormodule/mango/mangojuror.py
+++ jurormodule/mango/mangojuror.py
@@ -6,7 +6,7 @@ class MangoJuror(torch.nn.Module):
     def __init__(self, backbonenet):
         super(MangoJuror, self).__init__()
         self.backbonenet = backbonenet
-        self.l1 = torch.nn.Linear(1000,3)
+        self.l1 = torch.nn.Linear(1000, 4) # Three categories plus a confidence
 
     def forward(self, x):
         return torch.nn.functional.softmax(self.l1(self.backbonenet(x)), 1)
